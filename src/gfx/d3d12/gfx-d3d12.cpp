8eb01bef5d4bf8d445bcf89fdd5094adb8bfc05f
Kyle Weicht <kyle@awesomekyle.com>
2017-04-05 00:28:20 -0700

    Make D3D12 cpp to try to reduce intense masochism

--- src/gfx/d3d12/gfx-d3d12.cpp
+++ src/gfx/d3d12/gfx-d3d12.cpp
@@ -1,6 +1,13 @@
+extern "C" {
 #include "gfx/gfx.h"
+} // extern "C"
 #include <stdbool.h>
 #include <stdlib.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
+#if defined(_DEBUG)
+    #include <dxgidebug.h>
+#endif
 
 struct Gfx {
     bool initialized;
