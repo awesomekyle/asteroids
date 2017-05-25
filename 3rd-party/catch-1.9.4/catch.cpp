3d2008bbb7f6f249f9bbf4aca3ca7eb8ac332971
Kyle Weicht <kyle@awesomekyle.com>
2017-05-25 00:04:53 -0700

    Add initial stub test

--- 3rd-party/catch-1.9.4/catch.cpp
+++ 3rd-party/catch-1.9.4/catch.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN
+#define DO_NOT_USE_WMAIN
 #if defined(_WIN32)
-#   define CATCH_CONFIG_WINDOWS_CRTDBG
+    #define CATCH_CONFIG_WINDOWS_CRTDBG
 #endif // _WIN32
 #include "catch.hpp"
---
+++ test/graphics/graphics-test.cpp
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+
+//#include "graphics/graphics.h"
+
+namespace {
+
+//TEST_CASE("graphics lifetime")
+//{
+//    SECTION("graphics device creation") {
+//        WHEN("a graphics device is created") {
+//            ak::Graphics graphicsDevice();
+//            THEN("the device is successfully initialized") {
+//                REQUIRE(graphicsDevice.initialized() == true);
+//            }
+//        }
+//    }
+//}
+
+} // anonymous
