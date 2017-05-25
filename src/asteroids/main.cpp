e4e70f98364b1a5949da0deb5a68f85883f5e2c9
Kyle Weicht <kyle@awesomekyle.com>
2017-05-24 23:25:46 -0700

    Link to GLFW

--- src/asteroids/main.cpp
+++ src/asteroids/main.cpp
@@ -1,10 +1,30 @@
 #include <iostream>
 #include <gsl\span>
+#include <GLFW/glfw3.h>
 
-int main(int const argc, char const* const argv[])
+namespace {
+
+constexpr int kInitialWidth = 1920;
+constexpr int kInitialHeight = 1080;
+
+////
+// GLFW Callbacks
+////
+void glw_error_callback(int const error, char const* description)
 {
-    for (auto const* const arg : gsl::make_span(argv, argc)) {
-        std::cout << arg << std::endl;
+    std::cout << "GLW Error: " << error << " - " <<  description << std::endl;
+}
+
+} // anonymous
+
+int main(int const /*argc*/, char const* const /*argv*/[])
+{
+    // Initialize GLFW
+    glfwSetErrorCallback(::glw_error_callback);
+    if (glfwInit() != GLFW_TRUE) {
+        std::cerr << "GLFW Initialization Failed!" << std::endl;
+        return 1;
     }
+    std::cout << "GLFW initialization succeeded: " << glfwGetVersionString() << "\n";
     return 0;
 }
