42a485d332c236b0b34f23333394c1b25f09c106
Kyle Weicht <kyle@awesomekyle.com>
2017-05-25 22:06:39 -0700

    Add clang format file

--- src/graphics/graphics-d3d12.cpp
+++ src/graphics/graphics-d3d12.cpp
@@ -1,14 +1,14 @@
-#include "graphics\graphics.h"
 #include "graphics-d3d12.h"
+#include "graphics\graphics.h"
 
-#include <cassert>
 #include <array>
+#include <cassert>
 #include <gsl/gsl_assert>
 
 #include <d3d12.h>
 #include <dxgi1_6.h>
 #if defined(_DEBUG)
-    #include <dxgidebug.h>
+#include <dxgidebug.h>
 #endif
 #include <atlbase.h>
 
@@ -18,12 +18,13 @@
 
 namespace {
 
-struct DescriptorHeap {
-    CComPtr<ID3D12DescriptorHeap>   heap = {};
-    D3D12_DESCRIPTOR_HEAP_DESC      desc = {};
-    D3D12_DESCRIPTOR_HEAP_TYPE      type = {};
-    CD3DX12_CPU_DESCRIPTOR_HANDLE   cpu_start = {};
-    CD3DX12_GPU_DESCRIPTOR_HANDLE   gpu_start = {};
+struct DescriptorHeap
+{
+    CComPtr<ID3D12DescriptorHeap> heap = {};
+    D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+    D3D12_DESCRIPTOR_HEAP_TYPE type = {};
+    CD3DX12_CPU_DESCRIPTOR_HANDLE cpu_start = {};
+    CD3DX12_GPU_DESCRIPTOR_HANDLE gpu_start = {};
     UINT handleSize = {};
 
     inline D3D12_CPU_DESCRIPTOR_HANDLE cpu_slot(int slot)
@@ -34,10 +35,7 @@ struct DescriptorHeap {
     {
         return CD3DX12_GPU_DESCRIPTOR_HANDLE(gpu_start, slot, handleSize);
     }
-    operator ID3D12DescriptorHeap* ()
-    {
-        return heap;
-    }
+    operator ID3D12DescriptorHeap*() { return heap; }
 };
 
 template<typename T>
@@ -58,7 +56,8 @@ void set_name(CComPtr<D> object, char const* format, ...)
         va_start(args, format);
         vsnprintf(buffer, sizeof(buffer), format, args);
         va_end(args);
-        object->SetPrivateData(WKPDID_D3DDebugObjectName, (UINT)strnlen(buffer, sizeof(buffer)), buffer);
+        object->SetPrivateData(WKPDID_D3DDebugObjectName, (UINT)strnlen(buffer, sizeof(buffer)),
+                               buffer);
     }
 }
 
@@ -70,7 +69,8 @@ CComPtr<I> create_dxgi_debug_interface()
         return nullptr;
     }
 
-    auto const pDXGIGetDebugInterface = GetProcAddressSafe<decltype(&DXGIGetDebugInterface)>(dxgi_debugModule, "DXGIGetDebugInterface");
+    auto const pDXGIGetDebugInterface = GetProcAddressSafe<decltype(&DXGIGetDebugInterface)>(
+        dxgi_debugModule, "DXGIGetDebugInterface");
     if (pDXGIGetDebugInterface == nullptr) {
         return nullptr;
     }
@@ -102,17 +102,17 @@ DescriptorHeap CreateDescriptorHeap(ID3D12Device* const device,
     return heap;
 }
 
-} // anonymous namespace
-
+}  // anonymous namespace
 
 namespace ak {
 
 class GraphicsD3D12 : public Graphics
 {
-  public:
+   public:
     GraphicsD3D12()
+        : _device(nullptr)
+        , _render_queue(nullptr)
     {
-        // Create objects
         create_debug_interfaces();
         create_factory();
         find_adapters();
@@ -120,50 +120,43 @@ class GraphicsD3D12 : public Graphics
         create_queue();
         create_descriptor_heaps();
     }
-    ~GraphicsD3D12()
-    {
-        wait_for_idle();
-    }
+    ~GraphicsD3D12() { wait_for_idle(); }
 
-    bool create_swap_chain(void* window, void* /*application*/) final {
+    bool create_swap_chain(void* window, void* /*application*/) final
+    {
         Expects(_device);
         Expects(_render_queue);
         Expects(_factory);
 
-        if (window == nullptr)
-        {
+        if (window == nullptr) {
             return false;
         }
 
         HWND hwnd = static_cast<HWND>(window);
         constexpr DXGI_SWAP_CHAIN_DESC1 swap_chain_desc = {
-            0, // Width
-            0, // Height
-            DXGI_FORMAT_B8G8R8A8_UNORM, // Format
-            FALSE, // Stereo
+            0,                           // Width
+            0,                           // Height
+            DXGI_FORMAT_B8G8R8A8_UNORM,  // Format
+            FALSE,                       // Stereo
             {
-                1, // Count
-                0, // Quality
-            }, // SampleDesc
-            DXGI_USAGE_RENDER_TARGET_OUTPUT, // BufferUsage
-            kFramesInFlight, // BufferCount
-            DXGI_SCALING_NONE, // Scaling
-            DXGI_SWAP_EFFECT_FLIP_DISCARD, // SwapEffect
-            DXGI_ALPHA_MODE_UNSPECIFIED, // AlphaMode
-            0, // Flags
+                1,                            // Count
+                0,                            // Quality
+            },                                // SampleDesc
+            DXGI_USAGE_RENDER_TARGET_OUTPUT,  // BufferUsage
+            kFramesInFlight,                  // BufferCount
+            DXGI_SCALING_NONE,                // Scaling
+            DXGI_SWAP_EFFECT_FLIP_DISCARD,    // SwapEffect
+            DXGI_ALPHA_MODE_UNSPECIFIED,      // AlphaMode
+            0,                                // Flags
         };
         CComPtr<IDXGISwapChain1> swapchain1 = nullptr;
-        HRESULT hr = _factory->CreateSwapChainForHwnd(_render_queue,
-                                                      hwnd,
-                                                      &swap_chain_desc,
-                                                      nullptr,
-                                                      nullptr,
-                                                      &swapchain1);
-        assert(SUCCEEDED(hr)&& "Could not create swap chain");
+        HRESULT hr = _factory->CreateSwapChainForHwnd(_render_queue, hwnd, &swap_chain_desc,
+                                                      nullptr, nullptr, &swapchain1);
+        assert(SUCCEEDED(hr) && "Could not create swap chain");
         hr = swapchain1->QueryInterface(IID_PPV_ARGS(&_swap_chain));
-        assert(SUCCEEDED(hr)&& "Could not get swapchain3");
+        assert(SUCCEEDED(hr) && "Could not get swapchain3");
         hr = _factory->MakeWindowAssociation(hwnd, DXGI_MWA_NO_ALT_ENTER);
-        assert(SUCCEEDED(hr)&& "Could not disable alt-enter");
+        assert(SUCCEEDED(hr) && "Could not disable alt-enter");
 
         set_name(_swap_chain, "DXGI Swap Chain");
         _swap_chain->GetDesc1(&_swap_chain_desc);
@@ -171,42 +164,39 @@ class GraphicsD3D12 : public Graphics
         return true;
     }
 
-    bool resize(int width, int height) final {
+    bool resize(int width, int height) final
+    {
         Expects(_swap_chain);
-        if (_swap_chain == nullptr)
-        {
+        if (_swap_chain == nullptr) {
             return false;
         }
         wait_for_idle();
 
-        for (auto& buffer : _back_buffers)
-        {
+        for (auto& buffer : _back_buffers) {
             buffer.Release();
         }
 
         HRESULT hr = _swap_chain->ResizeBuffers(0, 0, 0, DXGI_FORMAT_UNKNOWN, 0);
-        assert(SUCCEEDED(hr)&& "Could not resize buffers");
+        assert(SUCCEEDED(hr) && "Could not resize buffers");
 
         _swap_chain->GetDesc1(&_swap_chain_desc);
-        for (UINT ii = 0; ii < _swap_chain_desc.BufferCount; ++ii)
-        {
+        for (UINT ii = 0; ii < _swap_chain_desc.BufferCount; ++ii) {
             auto& buffer = _back_buffers[ii];
             hr = _swap_chain->GetBuffer(ii, IID_PPV_ARGS(&buffer));
             assert(SUCCEEDED(hr) && "Could not get back buffer");
             set_name(buffer, "Back Buffer %d", ii);
             auto const back_buffer_slot = _rtv_heap.cpu_slot(ii);
             constexpr D3D12_RENDER_TARGET_VIEW_DESC view_desc = {
-                DXGI_FORMAT_B8G8R8A8_UNORM_SRGB,
-                D3D12_RTV_DIMENSION_TEXTURE2D,
+                DXGI_FORMAT_B8G8R8A8_UNORM_SRGB, D3D12_RTV_DIMENSION_TEXTURE2D,
             };
             _device->CreateRenderTargetView(buffer, &view_desc, back_buffer_slot);
         }
         return true;
     }
 
-    bool present() final {
-        if (_swap_chain == nullptr)
-        {
+    bool present() final
+    {
+        if (_swap_chain == nullptr) {
             return false;
         }
 
@@ -217,7 +207,7 @@ class GraphicsD3D12 : public Graphics
         return SUCCEEDED(hr);
     }
 
-  private:
+   private:
     void create_debug_interfaces()
     {
 #if defined(_DEBUG)
@@ -227,11 +217,16 @@ class GraphicsD3D12 : public Graphics
             _dxgi_debug->EnableLeakTrackingForThread();
         }
         if (_dxgi_info_queue) {
-            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_ERROR, TRUE);
-            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_CORRUPTION, TRUE);
-            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_WARNING, FALSE);
-            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_INFO, FALSE);
-            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL, DXGI_INFO_QUEUE_MESSAGE_SEVERITY_MESSAGE, FALSE);
+            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL,
+                                                 DXGI_INFO_QUEUE_MESSAGE_SEVERITY_ERROR, TRUE);
+            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL,
+                                                 DXGI_INFO_QUEUE_MESSAGE_SEVERITY_CORRUPTION, TRUE);
+            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL,
+                                                 DXGI_INFO_QUEUE_MESSAGE_SEVERITY_WARNING, FALSE);
+            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL,
+                                                 DXGI_INFO_QUEUE_MESSAGE_SEVERITY_INFO, FALSE);
+            _dxgi_info_queue->SetBreakOnSeverity(DXGI_DEBUG_ALL,
+                                                 DXGI_INFO_QUEUE_MESSAGE_SEVERITY_MESSAGE, FALSE);
         }
 
         CComPtr<ID3D12Debug1> d3d12_debug;
@@ -241,7 +236,7 @@ class GraphicsD3D12 : public Graphics
             d3d12_debug->SetEnableGPUBasedValidation(TRUE);
             d3d12_debug->SetEnableSynchronizedCommandQueueValidation(TRUE);
         }
-#endif // _DEBUG
+#endif  // _DEBUG
     }
 
     void create_factory()
@@ -249,7 +244,7 @@ class GraphicsD3D12 : public Graphics
         constexpr UINT const factory_flags =
 #if defined(_DEBUG)
             DXGI_CREATE_FACTORY_DEBUG |
-#endif // _DEBUG
+#endif  // _DEBUG
             0;
         HRESULT const hr = CreateDXGIFactory2(factory_flags, IID_PPV_ARGS(&_factory));
         assert(SUCCEEDED(hr) && "Could not create factory");
@@ -279,7 +274,7 @@ class GraphicsD3D12 : public Graphics
 
             adapter3->QueryVideoMemoryInfo(0, DXGI_MEMORY_SEGMENT_GROUP_LOCAL,
                                            &adapter.memory_info);
-            char buffer[128] = { '\0' };
+            char buffer[128] = {'\0'};
             snprintf(buffer, sizeof(buffer), "%S", adapter.desc.Description);
             set_name(adapter3, buffer);
 
@@ -295,22 +290,16 @@ class GraphicsD3D12 : public Graphics
             if (adapter.adapter == nullptr) {
                 break;
             }
-            HRESULT hr = D3D12CreateDevice(adapter.adapter, D3D_FEATURE_LEVEL_11_0,
-                                           IID_PPV_ARGS(&_device));
+            HRESULT hr =
+                D3D12CreateDevice(adapter.adapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&_device));
             if (SUCCEEDED(hr)) {
                 constexpr D3D_FEATURE_LEVEL levels[] = {
-                    D3D_FEATURE_LEVEL_9_1,
-                    D3D_FEATURE_LEVEL_9_2,
-                    D3D_FEATURE_LEVEL_9_3,
-                    D3D_FEATURE_LEVEL_10_0,
-                    D3D_FEATURE_LEVEL_10_1,
-                    D3D_FEATURE_LEVEL_11_0,
-                    D3D_FEATURE_LEVEL_11_1,
-                    D3D_FEATURE_LEVEL_12_0,
-                    D3D_FEATURE_LEVEL_12_1
-                };
+                    D3D_FEATURE_LEVEL_9_1,  D3D_FEATURE_LEVEL_9_2,  D3D_FEATURE_LEVEL_9_3,
+                    D3D_FEATURE_LEVEL_10_0, D3D_FEATURE_LEVEL_10_1, D3D_FEATURE_LEVEL_11_0,
+                    D3D_FEATURE_LEVEL_11_1, D3D_FEATURE_LEVEL_12_0, D3D_FEATURE_LEVEL_12_1};
                 D3D12_FEATURE_DATA_FEATURE_LEVELS featureLevels = {_countof(levels), levels};
-                hr = _device->CheckFeatureSupport(D3D12_FEATURE_FEATURE_LEVELS, &featureLevels, sizeof(featureLevels));
+                hr = _device->CheckFeatureSupport(D3D12_FEATURE_FEATURE_LEVELS, &featureLevels,
+                                                  sizeof(featureLevels));
                 assert(SUCCEEDED(hr));
                 _feature_level = featureLevels.MaxSupportedFeatureLevel;
                 _current_adapter = &adapter;
@@ -325,11 +314,8 @@ class GraphicsD3D12 : public Graphics
     {
         Expects(_device);
         constexpr D3D12_COMMAND_QUEUE_DESC queue_desc = {
-            D3D12_COMMAND_LIST_TYPE::D3D12_COMMAND_LIST_TYPE_DIRECT,
-            0,
-            D3D12_COMMAND_QUEUE_FLAGS::D3D12_COMMAND_QUEUE_FLAG_NONE,
-            0
-        };
+            D3D12_COMMAND_LIST_TYPE::D3D12_COMMAND_LIST_TYPE_DIRECT, 0,
+            D3D12_COMMAND_QUEUE_FLAGS::D3D12_COMMAND_QUEUE_FLAG_NONE, 0};
         HRESULT hr = _device->CreateCommandQueue(&queue_desc, IID_PPV_ARGS(&_render_queue));
         assert(SUCCEEDED(hr) && "Could not create queue");
         set_name(_render_queue, "Render Queue");
@@ -341,10 +327,8 @@ class GraphicsD3D12 : public Graphics
 
     void create_descriptor_heaps()
     {
-        constexpr D3D12_DESCRIPTOR_HEAP_DESC heap_desc = {
-            D3D12_DESCRIPTOR_HEAP_TYPE_RTV, 128,
-            D3D12_DESCRIPTOR_HEAP_FLAG_NONE, 0
-        };
+        constexpr D3D12_DESCRIPTOR_HEAP_DESC heap_desc = {D3D12_DESCRIPTOR_HEAP_TYPE_RTV, 128,
+                                                          D3D12_DESCRIPTOR_HEAP_FLAG_NONE, 0};
         _rtv_heap = CreateDescriptorHeap(_device, heap_desc, "RTV Heap");
     }
 
@@ -362,47 +346,47 @@ class GraphicsD3D12 : public Graphics
     // Constants
     //
     static constexpr size_t kMaxAdapters = 4;
-    static constexpr UINT   kFramesInFlight = 3;
+    static constexpr UINT kFramesInFlight = 3;
 
     //
     // Types
     //
-    struct Adapter {
-        CComPtr<IDXGIAdapter3>          adapter = {};
-        DXGI_ADAPTER_DESC2              desc = {};
-        DXGI_QUERY_VIDEO_MEMORY_INFO    memory_info = {};
+    struct Adapter
+    {
+        CComPtr<IDXGIAdapter3> adapter = {};
+        DXGI_ADAPTER_DESC2 desc = {};
+        DXGI_QUERY_VIDEO_MEMORY_INFO memory_info = {};
     };
 
     //
     // Data members
     //
-    CComPtr<IDXGIFactory4>  _factory;
+    CComPtr<IDXGIFactory4> _factory;
     std::array<Adapter, kMaxAdapters> _adapters;
     Adapter* _current_adapter;
 
-    CComPtr<ID3D12Device>       _device;
-    D3D_FEATURE_LEVEL           _feature_level;
+    CComPtr<ID3D12Device> _device;
+    D3D_FEATURE_LEVEL _feature_level;
     CComPtr<ID3D12CommandQueue> _render_queue;
-    CComPtr<ID3D12Fence>        _render_fence;
-    uint64_t                    _last_fence_completion;
+    CComPtr<ID3D12Fence> _render_fence;
+    uint64_t _last_fence_completion;
 
-    DescriptorHeap  _rtv_heap;
+    DescriptorHeap _rtv_heap;
 
     CComPtr<IDXGISwapChain3> _swap_chain;
     DXGI_SWAP_CHAIN_DESC1 _swap_chain_desc;
     std::array<CComPtr<ID3D12Resource>, kFramesInFlight> _back_buffers;
 
 #if defined(_DEBUG)
-    CComPtr<IDXGIDebug1>        _dxgi_debug;
-    CComPtr<IDXGIInfoQueue>     _dxgi_info_queue;
-    CComPtr<ID3D12InfoQueue>    d3d12InfoQueue;
-#endif // _DEBUG
+    CComPtr<IDXGIDebug1> _dxgi_debug;
+    CComPtr<IDXGIInfoQueue> _dxgi_info_queue;
+    CComPtr<ID3D12InfoQueue> d3d12InfoQueue;
+#endif  // _DEBUG
 };
-
 
 ScopedGraphics create_graphics_d3d12()
 {
     return std::make_unique<GraphicsD3D12>();
 }
 
-} // namespace ak
+}  // namespace ak
