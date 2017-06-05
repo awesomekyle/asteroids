0b6f06c0709f5fea38f515749969f0150ee02d3a
Kyle Weicht <kyle@awesomekyle.com>
2017-06-04 21:34:56 -0700

    Change depth store to dont care

--- src/graphics/vulkan/graphics-vulkan.cpp
+++ src/graphics/vulkan/graphics-vulkan.cpp
@@ -914,7 +914,7 @@ void GraphicsVulkan::create_render_passes()
             VK_FORMAT_D32_SFLOAT,                              // format
             VK_SAMPLE_COUNT_1_BIT,                             // samples
             VK_ATTACHMENT_LOAD_OP_CLEAR,                       // loadOp
-            VK_ATTACHMENT_STORE_OP_STORE,                      // storeOp
+            VK_ATTACHMENT_STORE_OP_DONT_CARE,                  // storeOp
             VK_ATTACHMENT_LOAD_OP_DONT_CARE,                   // stencilLoadOp
             VK_ATTACHMENT_STORE_OP_DONT_CARE,                  // stencilStoreOp
             VK_IMAGE_LAYOUT_UNDEFINED,                         // initialLayout
